88c8639b43d62739f5706a627d584907a86245f3
shum <shum@email.arizona.edu>
2019-07-02 18:59:47 -0600

    Add paths to desktop so Matlab can load paths for execution

---
+++ TestPaths/Path1/GeneratedCode/C++/Path.cpp
@@ -0,0 +1,23 @@
+// Generated from:
+//
+//
+
+#include <iostream>
+#include <string>
+#include <set>
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "State machine for PathA. Type 'exit' to exit the program any time." << std::endl;
+
+    std::cout << "Motion: Straight" << std::end;
+    std::cout << "Motion: Straight" << std::end;
+    std::cout << "Motion: Left" << std::end;
+    std::cout << "Motion: Left" << std::end;
+    std::cout << "Motion: ZigZagRight" << std::end;
+    std::cout << "Motion: Straight" << std::end;
+
+
+    return 0;
+}
---
+++ TestPaths/Path2/GeneratedCode/C++/Path.cpp
@@ -0,0 +1,20 @@
+// Generated from:
+//
+//
+
+#include <iostream>
+#include <string>
+#include <set>
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "State machine for aPath. Type 'exit' to exit the program any time." << std::endl;
+
+    std::cout << "Motion: Right" << std::end;
+    std::cout << "Motion: Left" << std::end;
+    std::cout << "Motion: Straight" << std::end;
+
+
+    return 0;
+}
