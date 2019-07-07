3c870e2a508f079f4fba2d75d50118b45d539cb6
shum <shum@email.arizona.edu>
2019-07-07 17:36:35 -0600

    Added test for control blocks all commands and variations

---
+++ TestPaths/ControlTest/GeneratedCode/C++/Path.cpp
@@ -0,0 +1,20 @@
+// Generated from:
+//
+//
+
+#include <iostream>
+#include <string>
+#include <set>
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "State machine for aPath. Type 'exit' to exit the program any time." << std::endl;
+
+    std::cout << "Motion: While" << std::end;
+    std::cout << "Motion: If" << std::end;
+    std::cout << "Motion: For" << std::end;
+
+
+    return 0;
+}
