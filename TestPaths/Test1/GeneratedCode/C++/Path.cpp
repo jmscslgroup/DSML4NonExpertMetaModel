2b238d90ec08a9aecb8c061e647a05cb43a81df8
shum <shum@email.arizona.edu>
2019-07-07 17:37:11 -0600

    Complex path to test all commands

---
+++ TestPaths/Test1/GeneratedCode/C++/Path.cpp
@@ -0,0 +1,29 @@
+// Generated from:
+//
+//
+
+#include <iostream>
+#include <string>
+#include <set>
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "State machine for aPath. Type 'exit' to exit the program any time." << std::endl;
+
+    std::cout << "Motion: Right" << std::end;
+    std::cout << "Motion: ZigZagLeft" << std::end;
+    std::cout << "Motion: ZigZagRight" << std::end;
+    std::cout << "Motion: Straight" << std::end;
+    std::cout << "Motion: Right_Signal" << std::end;
+    std::cout << "Motion: Left_Signal" << std::end;
+    std::cout << "Motion: Left" << std::end;
+    std::cout << "Motion: Horn" << std::end;
+    std::cout << "Motion: Hazard" << std::end;
+    std::cout << "Motion: For" << std::end;
+    std::cout << "Motion: If" << std::end;
+    std::cout << "Motion: While" << std::end;
+
+
+    return 0;
+}
