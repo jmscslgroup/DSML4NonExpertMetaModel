3267c32322053e13a11f35a26d89fb3951b960f7
shum <shum@email.arizona.edu>
2019-07-07 17:34:57 -0600

    Fixed syntax error for sensor param

--- Matlab ROS controllers/Matlab ROS controllers/While.m
+++ Matlab ROS controllers/Matlab ROS controllers/While.m
@@ -1,25 +1,25 @@
 function done = While(velpub,velsub,senssub,condition,codeToExecute)
-    straightData = [1,0,1,12.192,0];
-leftTurnData = [2,12.192,1,0,90];
-rightTurnData = [3,12.192,1,0,90];
-zigzagTurnData = [3, 6.096, 1, 0, 90];
-zigzagTurnData1 = [2, 6.096, 1, 0, 90];
+straightData = [1,0,1*1.25,12.192*(40+40)/(40+40+4),0];
+leftTurnData = [2,12.192*(40+40)/(40+40+4)*1.0,1*1.25,0,90];
+rightTurnData = [3,12.192*(40+40)/(40+40+4)*1.0,1*1.25,0,90];
+zigzagTurnData = [3, 6.096*(40+40)/(40+40+4)*1.0, .75*1.25, 0, 90];
+zigzagTurnData1 = [2, 6.096*(40+40)/(40+40+4)*1.0, .75*1.25, 0, 90];
 if(condition==1) 
         while(SafeLeft(senssub,abs(leftTurnData(2)) * 90*pi/180))
             for(k=1:length(codeToExecute))
         code = codeToExecute(k);
         if(code==1)
-            StraightController_NOGPS(velpub,velsub,sensorsub,straightData);
+            StraightController_NOGPS(velpub,velsub,senssub,straightData);
         elseif(code==2)
-            TurnController_NOGPS(velpub,velsub,sensorsub,leftTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,leftTurnData);
         elseif(code==3)
-            TurnController_NOGPS(velpub,velsub,sensorsub,rightTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,rightTurnData);
         elseif(code==4)
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData);
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData1);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData1);
         elseif(code==5) 
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData1);
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData1);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData);
         elseif(code==9)
             Horn();
         elseif(code==10)
@@ -36,17 +36,17 @@ if(condition==1)
            for(k=1:length(codeToExecute))
         code = codeToExecute(k);
         if(code==1)
-            StraightController_NOGPS(velpub,velsub,sensorsub,straightData);
+            StraightController_NOGPS(velpub,velsub,senssub,straightData);
         elseif(code==2)
-            TurnController_NOGPS(velpub,velsub,sensorsub,leftTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,leftTurnData);
         elseif(code==3)
-            TurnController_NOGPS(velpub,velsub,sensorsub,rightTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,rightTurnData);
         elseif(code==4)
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData);
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData1);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData1);
         elseif(code==5) 
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData1);
-            TurnController_NOGPS(velpub,velsub,sensorsub,zigzagTurnData);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData1);
+            TurnController_NOGPS(velpub,velsub,senssub,zigzagTurnData);
         elseif(code==9)
             Horn();
         elseif(code==10)
