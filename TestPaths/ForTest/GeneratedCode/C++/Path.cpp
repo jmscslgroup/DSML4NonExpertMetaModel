f065c37ead2881b4d8cbb7882c738633c798770f
shum <shum@email.arizona.edu>
2019-07-07 17:35:57 -0600

    Added test for For loop iteration amount

---
+++ TestPaths/ForTest/GeneratedCode/C++/Path.cpp
@@ -0,0 +1,18 @@
+// Generated from:
+//
+//
+
+#include <iostream>
+#include <string>
+#include <set>
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "State machine for aPath. Type 'exit' to exit the program any time." << std::endl;
+
+    std::cout << "Motion: For" << std::end;
+
+
+    return 0;
+}
